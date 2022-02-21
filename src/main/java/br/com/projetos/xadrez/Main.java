ea4325f7fc3ae15b4d317a25735e4792b453c40e
Lucas Brenner <lucasbref82@gmail.com>
2022-02-21 15:18:55 -0300

    Primeira classe: posição

--- src/main/java/br/com/projetos/xadrez/Main.java
+++ src/main/java/br/com/projetos/xadrez/Main.java
@@ -1,10 +1,12 @@
 package br.com.projetos.xadrez;
 
+import br.com.projetos.xadrez.tabuleiro.Posicao;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello World !");
+        Posicao posicao = new Posicao(3,5);
+        System.out.println(posicao);
 
     }
 }
